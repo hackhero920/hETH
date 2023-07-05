4118ebde1a59a6289ed6a5fdf145fe7943892093
lucasjustin <lucasjustin920@gmail.com>
2023-07-05 09:20:52 +0500

    Initial commit

---
+++ test/HuffWETH.t.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.15;
+
+import "../src/IWETH.sol";
+import "foundry-huff/HuffDeployer.sol";
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+contract HuffWethTest is Test {
+    IWETH public hETH;
+
+    function setUp() public {
+        hETH = IWETH(HuffDeployer.deploy("HuffWETH"));
+    }
+
+    function testName() public {
+        assertEq("Huff Wrapped ETH", hETH.name());
+    }
+
+    function testSymbol() public {
+        assertEq("HETH", hETH.symbol());
+    }
+
+    function testDecimals() public {
+        assertEq(18, hETH.decimals());
+    }
+}
